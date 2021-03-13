836762d5eb48936580f6d3f3cbc8f58e3b11aee1
Nitin Kumar <chdnitin007@gmail.com>
2021-03-13 21:51:31 +0530

    commit displayed on home page

--- controllers/home-controllers.js
+++ controllers/home-controllers.js
@@ -17,7 +17,15 @@ module.exports.home = function(req, res){
 
     //populating the user of posts
 
-    Post.find({}).populate('user').exec(function(err , posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path : 'comments' ,
+        populate : {
+            path : 'user' 
+        }
+    })
+    .exec(function(err , posts){
      
            return res.render('home' , {
            title : 'Codeial | Home ' ,
