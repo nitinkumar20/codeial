4dc925f48a37f3895ef4049a96823e86bbde2b64
Nitin Kumar <chdnitin007@gmail.com>
2021-03-13 16:00:40 +0530

    authentication added for posting

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const passport = require('passport');
 const router = express.Router();
 
 const postsController = require('../controllers/posts-controllers');
 
-router.post('/create/' , postsController.create);
+router.post('/create/' ,passport.checkAuthentication ,  postsController.create);
 
 module.exports = router ;
