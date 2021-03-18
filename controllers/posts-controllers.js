bffd9d9ca9084f0badcd3dd3900d66ce27ccb502
Nitin Kumar <chdnitin007@gmail.com>
2021-03-18 22:59:39 +0530

    post creation and deletion  code converted to ajax

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -5,11 +5,27 @@ module.exports.create = async function(req, res)
 {
   
     try{
-       await Post.create({
+      let post = await Post.create({
             content : req.body.content ,
             user : req.user._id 
             
         });
+
+        if (req.xhr){
+            // if we want to populate just the name of the user (we'll not want to send the password in the API), this is how we do it!
+            post = await post.populate('user', 'name').execPopulate();
+
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post created!"
+            });
+        }
+
+
+
+
         req.flash('success', 'Post published !');
         return res.redirect('back');    
     }
@@ -35,6 +51,22 @@ module.exports.destroy = async function(req , res)
               post.remove();
 
             await  Comment.deleteMany({post : req.params.id });
+
+            if(req.xhr){
+                return res.status(200).json({
+
+                    data : {
+                        post_id : req.params.id
+                    },
+
+                    message : "Post deleted ! "
+
+                });
+            }
+
+  
+
+
             req.flash('success', 'Post and associated comment deleted !');
             return res.redirect('back');
             
