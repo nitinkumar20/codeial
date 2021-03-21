10606483305dc06868c1b554d4c15a1b8c65315d
Nitin Kumar <chdnitin007@gmail.com>
2021-03-21 13:04:50 +0530

    version v2 added for api

--- controllers/api/v2/post_api.js
+++ controllers/api/v2/post_api.js
@@ -1,7 +1,7 @@
 module.exports.index = function(req, res){
     return res.json(200, {
 
-     message : " Inside version v1" ,
+     message : " Inside version v2" ,
      posts : []
 
     });
--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const postApi = require('../../../controllers/api/v1/post_api');
+const postApi = require('../../../controllers/api/v2/post_api');
 
 router.get('/', postApi.index);
 
 module.exports = router ;
-
