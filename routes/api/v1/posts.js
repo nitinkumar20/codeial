17ff5d623cf873e26153f96ae84e4f3ef3b69a38
Nitin Kumar <chdnitin007@gmail.com>
2021-03-21 11:58:35 +0530

    version v1 of project created for api

--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const postApi = require('../../../controllers/api/v1/post_api');
 
-router.use('/v1' ,require('./v1') );
-
+router.get('/', postApi.index);
 
 module.exports = router ;
