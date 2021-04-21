4041f90cf60f03239301d4bd667e5a6e106459a6
Nitin Kumar <chdnitin007@gmail.com>
2021-04-21 23:10:21 +0530

    set up nodemailer and config file

---
+++ config/nodemailer.js
@@ -0,0 +1,45 @@
+const nodemailer = require('nodemailer');
+
+const ejs = require('ejs');
+
+const path = require('path');
+
+
+let transporter = nodemailer.createTransport({
+
+   service : 'gmail.com' ,
+   host : 'smtp.gmail.com' ,
+   port : 587 ,
+   secure : false ,
+   auth : {
+
+    user : 'chdnitin007@gmail.com',
+    pass : 'Mnnit@123'
+
+   }
+
+});
+
+
+let renderTemplate = (data , relativePath) =>
+{  
+
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname ,'../views/mailers', relativePath) ,
+        data ,
+        function(err , template)
+        {
+            if(err){console.log('error in rendering template'); return ;}
+
+             mailHTML = template;
+        }
+    )
+   
+    return mailHTML;
+};
+
+module.exports = {
+    transporter : transporter ,
+    renderTemplate : renderTemplate 
+}
