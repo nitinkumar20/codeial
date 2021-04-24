811a5e0ea156e8405cacad9fe9a7f293768d52e2
Nitin Kumar <chdnitin007@gmail.com>
2021-04-24 12:51:52 +0530

    Like schema added

--- models/post.js
+++ models/post.js
@@ -17,8 +17,16 @@ const postSchema = new mongoose.Schema({
             ref : 'Comment'
         }
         
-    ]
+    ] ,
 
+
+    likes : [
+        {
+            type : mongoose.Schema.Types.ObjectId ,
+            ref : 'Like'
+        }
+        
+    ]
 
 } ,  {
     timestamps : true 
